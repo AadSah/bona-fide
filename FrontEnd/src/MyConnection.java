aaaca1b3037e505c9ce641160ad8d3cf97217d0c
t-aayu <aimayush@gmail.com>
2019-04-04 11:29:37 +0530

    New Front End

---
+++ FrontEnd/src/MyConnection.java
@@ -0,0 +1,20 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MyConnection {
+    
+        public static Connection getConnection(){
+     
+        Connection con = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/java_login_register", "ayush", "ayush");
+        } catch (Exception ex) {
+        	
+            System.out.println("Error");
+        }
+        
+        return con;
+    }
+    
+}
